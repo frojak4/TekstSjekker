488d9a2bf6a685807ba08dcd6062f49c1257691a
frojak4 <Frojak4@gmail.com>
2024-05-21 13:02:59 +0200

    Initial commit

---
+++ TekstSjekker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TekstSjekker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+                Console.Clear();
+                Console.WriteLine("Skriv inn din tekst!");
+                string userInput = Console.ReadLine();
+
+                Console.WriteLine($"Palindrom: {Utility.palindrome(userInput)}");
+                Console.WriteLine($"Reversed: {Utility.reversed(userInput)}");
+                Console.ReadKey(true);
+
+            }
+        }
+    }
+}
---
+++ TekstSjekker/Utility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekstSjekker
+{
+    internal class Utility
+    {
+
+
+        public static bool palindrome(string tekst)
+        {
+            bool state = true;
+            tekst = tekst.ToLower();
+
+            for (int i = 0; i < tekst.Length / 2; i++)
+            {
+                if (tekst[i] != tekst[tekst.Length - i - 1])
+                {
+                    state = false;
+                }
+            }
+
+
+            return state;
+        }
+
+        public static string reversed(string tekst)
+        {
+            string reversedString = "";
+
+            for (int i = tekst.Length; i > 0; i--)
+            {
+                reversedString += tekst[i - 1];
+            }
+
+            return reversedString;
+        }
+    }
+}
